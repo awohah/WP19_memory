6cf47aef0033c0a43e37d93c895c310c174add4d
awohah <49945295+awohah@users.noreply.github.com>
2019-06-03 19:47:58 +0200

    Update signup.php
    Makes sure new user can't signup if username is already taken

--- signup.php
+++ signup.php
@@ -1,5 +1,3 @@
-
-
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,13 +32,24 @@
             </div>
 
             <?php
-            if (isset($_POST['submit'])){
-                if ($_POST['password'] != $_POST['confirmation'])
-                {echo "<div is='wrong-password'>Please make sure both fields are the same!</div>";
-                }
-                else {echo"<div id='signup-success'>You've been signed up!</div>";
-                }
-            }
+            $usernames = [];
+                if (isset($_POST['submit'])){
+                    $json_file = file_get_contents("data/data.json");
+                    $users = json_decode($json_file, true);
+
+                foreach ($users as $key => $value){
+                    array_push($usernames, $value['user_name']);}
+                    if (in_array($_POST['user'], $usernames)) {
+                        echo "<div is='taken_username'>The username you entered is already taken.</div>";
+                        }else{
+                        if ($_POST['password'] != $_POST['confirmation'])
+                        {echo "<div is='wrong-password'>Please make sure both password fields are the same!</div>";
+                        }
+                        else {echo"<div id='signup-success'>You've been signed up!</div>";
+                        }
+                    }
+                };
+
             ?>
 
             <button type="submit" name="submit">Sign up</button>
@@ -62,16 +71,14 @@
 
 <?php
 if (isset($_POST['submit'])) {
-// Read articles
+
+// get json data with user details
     $json_file = file_get_contents("data/data.json");
     $new_user = json_decode($json_file, true);
-
-
     array_push($new_user, [
         'user_name' => $_POST['user'],
         'password' => $_POST['password']
     ]);
-
 // Save to external file
     $json_file = fopen('data/data.json', 'w');
     fwrite($json_file, json_encode($new_user));
