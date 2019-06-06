1359ff7a272270ce441596b53379ac9c73574d71
mariekevdh <mariekevanderholt@gmail.com>
2019-06-06 03:14:48 +0200

    Initial commit

---
+++ scripts/visible.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['tile_id'])){
+	// Read cards
+ 	$json_file = file_get_contents("../data/cards.json");
+ 	$cards = json_decode($json_file, true);
+	
+	// Make tile visible
+	foreach ($cards as $key => $value){
+		if ($value['tile_id'] == $_POST['tile_id']){
+			$cards[$key]['visibility'] = "visible";
+		}
+	}
+
+	// Save to external file
+	$json_file = fopen('../data/cards.json', 'w');
+	fwrite($json_file, json_encode($cards));
+	fclose($json_file);
+}
