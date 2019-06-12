bf04820e9e29dd38590c7c999ac725e365d7e6ef
Carlijn Assen <c.m.a.assen@student.rug.nl>
2019-06-12 22:35:35 +0200

    made the path to the images right so it is useful for everyone

--- game_board.php
+++ game_board.php
@@ -1,17 +1,28 @@
+
 <?php
 if (isset($_POST['call_now'])){
+    session_start();
     // Read cards
-    $json_file = file_get_contents("../data/cards.json");
+    $json_file = file_get_contents("../data/game.json");
     $cards = json_decode($json_file, true);
     // Generate HTML
     $cards_html = "";
-    
-    foreach($cards as $key => $value){
-        $cards_html.= sprintf('<div  id="%s" class="tile btn btn-info m-1 p-1" style="padding-left:1px;" tile_id="%s"  >', $value['tile_id'], $value['tile_id']);
-        $cards_html.= sprintf('<div id="%s" class="%s"><img src="../fp/img/%s.jpg" style="width:180px;height:140px;" /></div>', $value['picture'], $value['visibility'], $value['picture']);
-        $cards_html.= '</div>';
-        $cards_html.= '<div> TEST = '.$_SESSION['id'].'</div>'
+    if (empty($_SESSION['user'])){
+        header("Location: index.php");
+    }
+
+    foreach ($cards as $item){
+        if($item['id'] == ($_SESSION['id'])){
+            foreach($item["cards"] as $value){
+
+                $cards_html.= sprintf('<div id="%s" class="tile btn btn-info m-1 p-1" tile_id="%s" onclick="match()"> ',$value['tile_id'], $value['tile_id']);
+                $cards_html.= sprintf('<div id="%s" class="%s"><img class="images"  src="./img/%s.jpg" align="middle" /></div>', $value['picture'], $value['visibility'], $value['picture']);
+                $cards_html.= '</div>';
+
+            }
+        }
     };
+
     // Save html into array
     $export_data = [
         'html' => $cards_html
@@ -21,3 +32,4 @@ if (isset($_POST['call_now'])){
     echo json_encode($export_data);
 }
 ?>
+
