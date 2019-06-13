22b8fa81746ba7b5a989be309cdbe8ef4ae36af6
larisabulbaai <larisabulbaai@hotmail.com>
2019-06-13 21:52:08 +0200

    edited play.php

--- redirect2.php
+++ redirect2.php
@@ -4,8 +4,7 @@ if(empty($_SESSION['user'])){
     header('location: index.php');
 }else{
     echo '<h1>Have fun playing '.$_SESSION['user'].'!</h1>';
-    echo '<a class="back" href="play.php">Back</a>';
-    echo '<a class="signout" href="logout.php">Sign out</a>';
+    
 
     include __DIR__ . '/html/memory.html';
 
