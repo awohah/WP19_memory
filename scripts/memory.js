071b4e3e74a6e760af85c8fa076176c3ae19820b
mariekevdh <mariekevanderholt@gmail.com>
2019-06-13 16:16:56 +0200

    modify game and remove repeated code

--- scripts/memory.js
+++ scripts/memory.js
@@ -1,3 +1,10 @@
+let clicked_cards = [];
+let tiles = [];
+let matches = 0;
+let round = 0;
+let score_even = 0;
+let score_uneven = 0;
+
 function newGame() {
     let new_game = $.post("scripts/new_game.php", {call_now: "True"});
     new_game.done(function (data) {
@@ -5,218 +12,83 @@ function newGame() {
     });
 }
 
-
-
 function print_cards() {
     let cards_html = $.post("scripts/game_board.php", {call_now: "True"});
     let game_container = $('#game_board');
     cards_html.done(function(data) {
         game_container.empty();
         game_container.append(data.html);
-        $('.tile').click(function () {
-            makeVisible(this);
-            console.log(this);
-
-
-        });
-
-
-
+        if (matches < 6) {
+            $('.tile').click(function () {
+                flipCard(this);
+            });
+        };
+        if (matches == 6) {
+            console.log("end");
+        }
     });
-
-
 }
-    var a = Array();
-    var b = Array();
-    let score_even = 0;
-    let score_uneven = 0;
-    let count_tiles= 0;
-    let turns = 0;
 
-    function match() {
-        let comb1 = ["7", "11"];
-        let comb2 = ["1", "5"];
-        let comb3 = ["2", "9"];
-        let comb4 = ["12", "6"];
-        let comb5 = ["8", "3"];
-        let comb6 = ["4", "10"];
-
-        $('.tile').click(function (event) {
-           console.log(score_uneven);
-           console.log(score_even);
-           turns = turns + 0.5;
-           console.log(turns);
-           console.log(turns % 2);
-
-            let ima = this;
-            b = b.concat(ima);
-            console.log(b);
-            let im = $(this).get(0)['id'];
-            a = a.concat(im);
-            if (a.length === 2) {
-                if (count_tiles === 12) {
-                    alert("stop the game");
+function flipCard(card) {
+    let tile_id = $(card).attr('tile_id');
+    let picture = $(card).attr('picture');
+    let turn_tile = $.post("scripts/flip.php", {tile_id: tile_id});
+    turn_tile.done(function (data) {
+        print_cards();
+    });
+    if (tile_id != tiles[tiles.length-1]){
+        clicked_cards.push(picture);
+        tiles.push(tile_id);
+        if (clicked_cards.length%2 == 0) {
+            round++;
+            if (clicked_cards[clicked_cards.length-2] == clicked_cards[clicked_cards.length-1]) {
+                $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
+                window.setTimeout(function () {
+                    makeInvisible(tiles[tiles.length-2]);
+                    makeInvisible(tiles[tiles.length-1]);
+                }, 500);
+                matches++;
+                if (round%2 == 0){
+                    score_even+=1;
+                } else {
+                    score_uneven+=1;
                 }
+            } else {
+                $('h1').text("wrong").attr("class", "alert alert-danger").css("width", "40%").css("margin", "auto");
+                window.setTimeout(function () {
+                    flipCardBack(tiles[tiles.length-2]);
+                    flipCardBack(tiles[tiles.length-1]);
+                }, 500);
+            };
+        };
+    };
+}
 
-                else if (a[0] === comb1[0] && a[1] === comb1[1] || a[0] === comb1[1] && a[1] === comb1[0]) {
-                    a.length = 0;
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    }
-                    else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    let card1 = document.getElementById("7");
-                    let card2 = document.getElementById("11");
-                    console.log(card1);
-                    console.log(card2);
+function flipCardBack(tile_id) {
+    let turn_tile = $.post("scripts/flip_back.php", {tile_id: tile_id});
+    turn_tile.done(function (data) {
+        print_cards();
+    });
+}
 
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
+function makeVisible(card) {
+    let tile_id = $(card).attr('tile_id');
+    let turn_tile = $.post("scripts/visible.php", {tile_id: tile_id});
+    turn_tile.done(function (data) {
+        print_cards();
+    });
+}
 
-                } else if (a[0] === comb2[0] && a[1] === comb2[1] || a[0] === comb2[1] && a[1] === comb2[0]) {
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    a.length = 0;
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    }
-                    else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
+function makeInvisible(tile_id) {
+    let turn_tile = $.post("scripts/invisible.php", {tile_id: tile_id});
+    turn_tile.done(function (data) {
+        print_cards();
+    });
+}
 
-                } else if (a[0] === comb3[0] && a[1] === comb3[1] || a[0] === comb3[1] && a[1] === comb3[0]) {
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    a.length = 0;
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    }
-                    else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
-
-                } else if (a[0] === comb4[0] && a[1] === comb4[1] || a[0] === comb4[1] && a[1] === comb4[0]) {
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    a.length = 0;
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    }
-                    else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
-
-                } else if (a[0] === comb5[0] && a[1] === comb5[1] || a[0] === comb5[1] && a[1] === comb5[0]) {
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    a.length = 0;
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    }
-                    else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
-
-
-                } else if (a[0] === comb6[0] && a[1] === comb6[1] || a[0] === comb6[1] && a[1] === comb6[0]) {
-                    $('h1').text("good job").attr("class", "alert alert-success").css("width", "40%").css("margin", "auto");
-                    console.log("good job");
-                    a.length = 0;
-                    if (turns % 2 === 0) {
-                        score_even = score_even + 1;
-                    } else {
-                        score_uneven = score_uneven + 1;
-                    }
-                    count_tiles = count_tiles + 2;
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    flip(b[0]);
-                    flip(b[1]);
-                    b.length = 0;
-                }
-
-
-
-                    else {
-                    $('h1').text("wrong").attr("class", "alert alert-danger").css("width", "40%").css("margin", "auto");
-                    console.log("wrong");
-                    makeInvisible(b[0]);
-                    makeInvisible(b[1]);
-                    a.length = 0;
-                    b.length = 0;
-                }
-
-
-
-            }
-        });
-    }
-
-
-
-        function  flip(card) {
-            let tile_id = $(card).attr('tile_id');
-            let turn_tile = $.post("scripts/flip.php", {tile_id: tile_id});
-            turn_tile.done(function (data) {
-                print_cards();
-            });
-        }
-
-
-
-        function makeVisible(card) {
-            let tile_id = $(card).attr('tile_id');
-            let turn_tile = $.post("scripts/visible.php", {tile_id: tile_id});
-            turn_tile.done(function (data) {
-                print_cards();
-            });
-        }
-
-        function makeInvisible(card) {
-            let tile_id = $(card).attr('tile_id');
-            let turn_tile = $.post("scripts/invisible.php", {tile_id: tile_id});
-            turn_tile.done(function (data) {
-                print_cards();
-            });
-        }
-
-        $(function() {
-            newGame();
-
-
-            window.setInterval(function () {
-                print_cards();
-            }, 2000);
-
-        });
+$(function() {
+    newGame();
+    window.setInterval(function () {
+        print_cards();
+    }, 2000);
+});
