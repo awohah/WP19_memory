a8ccdb5c31ce5817e114f41ecb47891c21a7bc1c
Carlijn Assen <c.m.a.assen@student.rug.nl>
2019-06-13 18:10:36 +0200

    added validation for signup.php

--- js/validation.js
+++ js/validation.js
@@ -1,39 +1,83 @@
-$(function() {
+function checkUser(){
+    let user_input = $('#user');
+    let cur_val = user_input.val();
+    let user_regex = "^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$";
+    if (cur_val.match(user_regex) && cur_val !== ''){
+        user_input.removeClass('is-invalid');
+        user_input.addClass('is-valid');
+        return true;
+    } else{
+        user_input.removeClass('is-valid');
+        user_input.addClass('is-invalid');
+        return false;
+    }
+}
+
+
+function checkPassword() {
+    let password_input = $('#password');
+    let cur_val = password_input.val();
+    let password_regex = "^(?=.*\\d).{4,8}$";
+    if (cur_val.match(password_regex) && cur_val !== '') {
+        password_input.removeClass('is-invalid');
+        password_input.addClass('is-valid');
+        return true;
+    } else {
+        password_input.removeClass('is-valid');
+        password_input.addClass('is-invalid');
+        return false;
+    }
+}
+
+
+
+
+function check () {
+
+
     $('#user').keyup(function () {
         checkUser();
     });
 
+
     $('#password').keyup(function () {
         checkPassword();
     });
+
+
+
+
+    $('#sub').click(function () {
+        console.log('hello');
+
+
+        if (checkUser() === false) {
+            $('#usr').show();
+            $('#user').text = "";
+
+        }
+
+
+        if (checkPassword() === false) {
+            $('#pw').show();
+            $('#password').text = "";
+
+        }
+
+
+        else {
+            $('.alert-danger').hide();
+            $('form').submit();
+            $('#button').click();
+        }
+    });
+
+
+
+}
+
+
+$(document).ready(function() {
+    check();
+
 });
-
-function checkUser(){
-    let user_input = $('#user');
-    let cur_val = user_input.val();
-    let user_regex = "/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/";
-    if (cur_val.match(user_regex) && cur_val !== ''){
-        user_input.removeClass('invalid');
-        user_input.addClass('valid');
-        return true;
-    } else{
-        user_input.removeClass('valid');
-        user_input.addClass('invalid');
-        return false;
-    }
-}
-
-function checkPassword(){
-    let password_input = $('#password');
-    let cur_val = password_input.val();
-    let password_regex = "/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/";
-    if (cur_val.match(password_regex) && cur_val !== ''){
-        password_input.removeClass('invalid');
-        password_input.addClass('valid');
-        return true;
-    } else{
-        password_input.removeClass('valid');
-        password_input.addClass('invalid');
-        return false;
-    }
-}
--- signup.php
+++ signup.php
@@ -1,26 +1,105 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+    <title>Sign-up</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/signin-up.css">
+    <script type="text/javascript" src="data/data.json"></script>
+    <script type="text/javascript" src="js/validation.js"></script>
+
+
+</head>
+
+<body id="signup-body">
+
+
+<div id="wrapper">
+    <div id="container">
+        <div id="usr" class="alert alert-danger" role="alert">
+            Please enter a correct username!
+        </div>
+
+        <div id="pw" class="alert alert-danger" role="alert">
+            Please enter a valid password!
+        </div>
+
+
+        <form>
+            <div class="signup-logo"></div>
+            <div class="signup-title">Sign up</div>
+
+            <div class="form-group">
+                <label for="user"></label>
+                <input type="text" class="form-control input" id="user" name="user" placeholder="Username">
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+                <div class="invalid-feedback">
+                    Username must contain at least one letter, at least one number, and be longer than six characters.
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password"></label>
+                <input type="password" class="form-control input" id="password" name="password" placeholder="Password">
+                <div class="valid-feedback">
+                    Looks good!
+                </div>
+                <div class="invalid-feedback">
+                    Password must be between 4 and 8 digits long and include at least one numeric digit.
+                </div>
+            </div>
+
+            <div class="form-group">
+                <input type="password"  class="form-control input" id="confirmation" name="confirmation" placeholder="Confirm your password" required>
+
+            </div>
+
+            <?php
+            $usernames = [];
+            if (isset($_POST['submit'])){
+                $json_file = file_get_contents("data/data.json");
+                $users = json_decode($json_file, true);
+
+                foreach ($users as $key => $value){
+                    array_push($usernames, $value['user_name']);}
+                if (in_array($_POST['user'], $usernames)) {
+                    echo "<div id='taken_username'>The username you entered is already taken</div>";
+                }else{
+                    if ($_POST['password'] != $_POST['confirmation'])
+                    {echo "<div id='wrong-password'>Please make sure both password fields are the same!</div>";
+                    }
+                    else {echo"<div id='signup-success'>You've been signed up!</div>";
+                    }
+                }
+            };
+            ?>
+
+
+            <div class="form-group">
+                <div id="sub" class="btn btn-primary btn-lg button"> Sign up!</div>
+                <button id="button" name="submit"  type="submit" class="btn btn-primary btn-lg button">button</button>
+            </div>
+
+        </form>
+    </div>
+
+    <div class="login">
+        <a href="index.php">Log in?</a>
+    </div>
+
+
+</body>
+
+</html>
+
 <?php
-include __DIR__ . '/tpl/head2.php'
-
-$usernames = [];
-if (isset($_POST['submit'])){
-    $json_file = file_get_contents("data/data.json");
-    $users = json_decode($json_file, true);
-
-    foreach ($users as $key => $value){
-        array_push($usernames, $value['user_name']);}
-    if (in_array($_POST['user'], $usernames)) {
-        echo "<div id='taken_username'>The username you entered is already taken</div>";
-    }else{
-        if ($_POST['password'] != $_POST['confirmation'])
-        {echo "<div id='wrong-password'>Please make sure both password fields are the same!</div>";
-        }
-        else {echo"<div id='signup-success'>You've been signed up!</div>";
-        }
-    }
-};
-
-include __DIR__ . '/tpl/footer2.php'
-
 if (isset($_POST['submit'])) {
 
 // get json data with user details
@@ -36,3 +115,4 @@ if (isset($_POST['submit'])) {
     fclose($json_file);
 }
 ?>
+
