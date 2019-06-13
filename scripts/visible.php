85665a6652483d8874a0c2f23b3326da807d1494
mariekevdh <mariekevanderholt@gmail.com>
2019-06-13 16:18:11 +0200

    remove flip=invisible

--- scripts/visible.php
+++ scripts/visible.php
@@ -1,26 +1,24 @@
 <?php
 if (isset($_POST['tile_id'])){
     session_start();
-	// Read cards
- 	$json_file = file_get_contents("../data/game.json");
- 	$cards = json_decode($json_file, true);
+    // Read cards
+    $json_file = file_get_contents("../data/game.json");
+    $cards = json_decode($json_file, true);
 
-	// Make tile visible
+    // Make tile visible
     foreach ($cards as $game => $game_value) {
         if ($game_value['id'] == ($_SESSION['id'])){
             foreach ($game_value["cards"] as $key => $value) {
                 if ($value['tile_id'] == $_POST['tile_id']) {
                     $cards[$game]["cards"][$key]['visibility'] = 'visible';
-                    $cards[$game]["cards"][$key]['flip'] = 'invisible';
-
                 }
             }
         }
     };
 
-	// Save to external file
-	$json_file = fopen('../data/game.json', 'w');
-	fwrite($json_file, json_encode($cards));
-	fclose($json_file);
+    // Save to external file
+    $json_file = fopen('../data/game.json', 'w');
+    fwrite($json_file, json_encode($cards));
+    fclose($json_file);
 }
 ?>
