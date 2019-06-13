412df2d8c85b71629f8a846fcdd1d116180682fa
Carlijn Assen <c.m.a.assen@student.rug.nl>
2019-06-13 19:54:53 +0200

    made the login page look like before

--- tpl/body.php
+++ tpl/body.php
@@ -10,14 +10,14 @@
             <div class="login-title">Login</div>
 
             <div class="form-group">
-                <input type="text" id="user" name="user" placeholder="Username" required>
+                <input type="text"class="form-control input" id="user" name="user" placeholder="Username" required>
             </div>
 
             <div class="form-group">
-                <input type="password"  id="password" name="password" placeholder="Password" required>
+                <input type="password" class="form-control input" id="password" name="password" placeholder="Password" required>
             </div>
 
-            <button type="submit" name="submit">Log in</button>
+            <button id="sub" type="submit" name="submit">Log in</button>
 
             <div class="signup">
                 <a href="signup.php">Sign up?</a>
--- tpl/head.php
+++ tpl/head.php
@@ -9,12 +9,12 @@
     <meta charset="utf-8">
 
     <title>Login</title>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/signin-up.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
+    <script type="text/javascript" src="data/data.json"></script>
+    <script type="text/javascript" src="js/validation.js"></script>
 
 </head>
 
