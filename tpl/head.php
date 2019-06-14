d1d82aea362f9a8ae23395795ece66ca90e0951e
larisabulbaai <larisabulbaai@hotmail.com>
2019-06-14 16:36:30 +0200

    Linked css in sigunp page, wasnt working.

--- tpl/head.php
+++ tpl/head.php
@@ -11,7 +11,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/signin-up.css">
+    <link rel="stylesheet" type="text/css" href="../css/signin-up.css">
     <script type="text/javascript" src="data/data.json"></script>
     <script type="text/javascript" src="js/validation.js"></script>
 
